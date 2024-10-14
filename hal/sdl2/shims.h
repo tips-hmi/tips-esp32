24843ede5a2d802d246273c91f0791b15f792384
李昕 <lixin.henry@bytedance.com>
2024-10-14 19:32:08 +0800

    chore: remove sdl2

--- hal/sdl2/shims.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <chrono>
-
-static uint64_t millis() {
-  return std::chrono::duration_cast<std::chrono::milliseconds>(
-             std::chrono::system_clock::now().time_since_epoch())
-      .count();
-}
